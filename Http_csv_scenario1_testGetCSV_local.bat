c2fb8e2dd87238ca813e07c5771fa59295f08b39
Andrew Etuk <awetuk@gmail.com>
2023-01-21 19:23:26 +0000

    updated

---
+++ Http_csv_scenario1_testGetCSV_local.bat
@@ -0,0 +1,23 @@
+java -Dlogging.level=ON -cp csvjdbc-1.0-37.jar;HashRelMap-0.0.1-SNAPSHOT-jar-with-dependencies.jar client.Ddsloader ^
+-licensekey HloadPL6Z-5UKS-RVKY-FEH1-ZQEX-IIFH-ZWTR-OLIU ^
+-sourcequery "SELECT userid as userid_dot_SAMP_, age as age_dot_SAMP_, EventId as EventId_dot_SAMP_, channel as channel_dot_SAMP_, breuserid as breuserid_dot_SAMP_, brecontext as brecontext_dot_SAMP_ FROM GetNextBestContent" ^
+-sourcereadintervalinimillisecs 5000 ^
+-targethttpmethod "GET" ^
+-targetdataformat "XML2" ^
+-restresponsefileloc "test.csv" ^
+-restresponsefileformat "CSV" ^
+-tablekeys "tablekey[0][0]=return userid;SAMP" ^
+-sourcedburl "jdbc:relique:csv:." ^
+-sourcedriver "org.relique.jdbc.csv.CsvDriver" ^
+-sourceusername "jdbc_source_username" ^
+-sourceuserpassword "jdbc_source_password" ^
+-targetkeys userid ^
+-targeturl "http://132.145.51.102:8080/bre/getdata?userid=[userid]&age=[age]&breuserid=[breuserid]&channel=[channel]&brecontext=[brecontext]" ^
+-targetdbtable data.ddstest_11b6429df8735b13d70808652d560be2 ^
+-targetusername dnode_ext ^
+-targetpassword dnode_ext ^
+-dbfetchsize 1 ^
+-bulkloaderthreads 1 ^
+-setdatasources "top_ eq SAMP" ^
+-setcolumnmappings "userid eq USERID;age eq AGE;channel eq CHANNEL;breuserid eq BREUSERID;brecontext eq BRECONTEXT"
+pause
--- Http_csv_scenario1_testTopic_local.bat
+++ Http_csv_scenario1_testTopic_local.bat
@@ -1,20 +1,22 @@
 java -Dlogging.level=ON -cp csvjdbc-1.0-37.jar;HashRelMap-0.0.1-SNAPSHOT-jar-with-dependencies.jar client.Ddsloader ^
--licensekey KloadNG92-2L52-OYMS-V5VU-6H8R-O7TS-STLP-I61A ^
+-licensekey HloadPL6Z-5UKS-RVKY-FEH1-ZQEX-IIFH-ZWTR-OLIU ^
 -sourcequery "SELECT CUSTID as CUSTIDTMP_dot_SAMP_, TRANSTYPE as TransType_dot_SAMP_, TRANSVALUE as TransValue_dot_SAMP_ FROM Sampleoutputforstreaming" ^
 -sourcereadintervalinimillisecs 5000 ^
 -targethttpmethod "POST" ^
+-targetdataformat "JSON_ROOT" ^
+-targetrootelement "message" ^
 -tablekeys "tablekey[0][0]=return CUSTIDTMP;SAMP" ^
 -sourcedburl "jdbc:relique:csv:." ^
 -sourcedriver "org.relique.jdbc.csv.CsvDriver" ^
 -sourceusername "jdbc_source_username" ^
 -sourceuserpassword "jdbc_source_password" ^
 -targetkeys CUSTIDTMP ^
--targeturl 192.168.56.101:9091 ^
+-targeturl http://132.145.51.102:8080/bre/postdatajsonp ^
 -targetdbtable data.ddstest_11b6429df8735b13d70808652d560be2 ^
 -targetusername dnode_ext ^
 -targetpassword dnode_ext ^
 -dbfetchsize 1 ^
 -bulkloaderthreads 1 ^
 -setdatasources "top_ eq SAMP" ^
--setcolumnmappings "CUSTIDTMP eq customerid"
+rem -setcolumnmappings "CUSTIDTMP eq customerid"
 pause
