08252d9f005313167d31d9acd7ba3f803916eaee
Andrew Etuk <awetuk@gmail.com>
2023-01-30 23:02:35 +0000

    updated

--- Http_csv_scenario1_testGetJSON_local.bat
+++ Http_csv_scenario1_testGetJSON_local.bat
@@ -1,5 +1,5 @@
 java -Dlogging.level=ON -cp csvjdbc-1.0-37.jar;HashRelMap-0.0.1-SNAPSHOT-jar-with-dependencies.jar client.Ddsloader ^
--licensekey HloadPL6Z-5UKS-RVKY-FEH1-ZQEX-IIFH-ZWTR-OLIU ^
+-licensekey HloadD216-YPET-2FH3-PM64-YIPT-89YM-OSUX-HZYB ^
 -sourcequery "SELECT userid as userid_dot_SAMP_, age as age_dot_SAMP_, EventId as EventId_dot_SAMP_, channel as channel_dot_SAMP_, breuserid as breuserid_dot_SAMP_, brecontext as brecontext_dot_SAMP_ FROM GetNextBestContent" ^
 -sourcereadintervalinimillisecs 5000 ^
 -targethttpmethod "GET" ^
@@ -8,13 +8,8 @@ java -Dlogging.level=ON -cp csvjdbc-1.0-37.jar;HashRelMap-0.0.1-SNAPSHOT-jar-wit
 -tablekeys "tablekey[0][0]=return userid;SAMP" ^
 -sourcedburl "jdbc:relique:csv:." ^
 -sourcedriver "org.relique.jdbc.csv.CsvDriver" ^
--sourceusername "jdbc_source_username" ^
--sourceuserpassword "jdbc_source_password" ^
 -targetkeys userid ^
 -targeturl "http://132.145.51.102:8080/bre/getdata?userid=[userid]&age=[age]&breuserid=[breuserid]&channel=[channel]&brecontext=[brecontext]" ^
--targetdbtable data.ddstest_11b6429df8735b13d70808652d560be2 ^
--targetusername dnode_ext ^
--targetpassword dnode_ext ^
 -dbfetchsize 1 ^
 -bulkloaderthreads 1 ^
 -setdatasources "top_ eq SAMP" ^
