827a95a6f485fbfe921121cffdd0b5a1d55cc29c
Andrew Etuk <awetuk@gmail.com>
2024-11-07 13:08:55 +0000

    Update Http_csv_scenario1_testGetCSV_NoMapping_local.sh

--- Http_csv_scenario1_testGetCSV_NoMapping_local.sh
+++ Http_csv_scenario1_testGetCSV_NoMapping_local.sh
@@ -1,6 +1,6 @@
 java -Dlogging.level=ON -cp csvjdbc-1.0-37.jar:HashRelMap-0.0.1-SNAPSHOT-jar-with-dependencies.jar client.Ddsloader \
--licensekey HloadHHWX-1YRW-9ZHV-1AV4-J46P-92VF-2648-S3SJ \
--sourcequery "SELECT userid as userid_dot_SAMP_, age as age_dot_SAMP_, EventId as EventId_dot_SAMP_, channel as channel_dot_SAMP_, breuserid as breuserid_dot_SAMP_, brecontext as brecontext_dot_SAMP_ FROM GetNextBestContent" \
+-licensekey HloadTrial7dayDCH2C2IICB4G242I1I8F48A38AD5CC81 \
+-sourcequery "SELECT userid as userid_dot_SAMP_, age as age_dot_SAMP_, EventId as EventId_dot_SAMP_, channel as channel_dot_SAMP_, breuserid as breuserid_dot_SAMP_, brecontext as brecontext_dot_SAMP_ FROM test" \
 -sourcereadintervalinimillisecs 5000 \
 -targethttpmethod "GET" \
 -targetdataformat "XML2" \
